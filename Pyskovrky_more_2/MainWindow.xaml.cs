e360904fbd5794c0ba811385f3cc7e2b2583383b
Karel Cveček <kastrolekkaja@seznam.cz>
2023-03-06 19:59:54 +0100

    asd

--- Pyskovrky_more_2/MainWindow.xaml.cs
+++ Pyskovrky_more_2/MainWindow.xaml.cs
@@ -20,18 +20,80 @@ namespace Pyskovrky_more_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Game game;
+
+        int cntx = 3;
+        int cnty = 3;
+
         public MainWindow()
         {
             InitializeComponent();
-            Game test = new Game(null, value.X);
+            game = new Game(null, value.X);
             for(int i = 1; i < 6; i++)
             {
-                test.add_field(new fullfield(2, i, value.X));
-                test.add_field(new fullfield(5, i, value.Y));
+                game.add_field(new fullfield(2, i, value.X));
+                game.add_field(new fullfield(5, i, value.Y));
             }
 
-            fullfield[,] field = test.fieldalize(test.FieldList[3], 3, 3);
+            display();
+        }
 
+        public void display()
+        {
+            int x = 5;
+            int y = 3;
+            fullfield cnt = new fullfield(cntx, cnty, value.X);
+            //tady ty parantery narvat do konstruktur a bude to dynamický
+
+            fullfield[,] field = game.fieldalize(cnt, x, y);
+            _out.Text = "";
+            for(int i = (y*2); i >= 0; i--)
+            {
+                for(int j = (x*2); j >= 0; j--)
+                {
+                    if (field[i, j] == null)
+                    {
+                        _out.Text += " _ ";
+                        if (j == 0)
+                        {
+                            _out.Text += "\n";
+                        }
+                    }
+                    else
+                    {
+                        _out.Text += " " + field[i, j].val.ToString() + " ";
+                        if (j == 0)
+                        {
+                            _out.Text += "\n";
+                        }
+                    }
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cntx++;
+            display();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            cntx--;
+            display();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            cnty++;
+            display();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            cnty--;
+            display();
         }
     }
 }
