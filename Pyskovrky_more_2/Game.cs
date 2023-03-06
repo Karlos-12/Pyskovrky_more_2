b6c2ca36efceb42a0578e7b96f36567bb082d3a4
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2023-03-06 13:18:40 +0100

    wer

--- Pyskovrky_more_2/Game.cs
+++ Pyskovrky_more_2/Game.cs
@@ -50,7 +50,47 @@ namespace Pyskovrky_more_2
 
         public void eval(fullfield temp)
         {
+            fullfield[,] area = new fullfield[9, 9];
+            foreach(fullfield n in FieldList)
+            {
+                if(n.x > temp.x -4 && n.x < temp.x +4 && n.y > temp.y -4 && n.y < temp.y +4)
+                {
+                    int vxn = n.x - temp.x;
+                    int vyn = n.y - temp.y;
+                    area[vyn, vxn] = n;
+                }
+            }
+
+            if( 5 <= area.Cast<fullfield>().Count(x => x != null))
+            {
+                int[] way = new int[] { 0, 0 };
+                for (int i = 0; i < 8; i++)
+                {
+                    switch(i)
+                    {
+                        case 0: way = new int[] { 0, 1 }; break;
+                        case 1: way = new int[] { 0, -1 }; break;
+                        case 2: way = new int[] { 1, 0 }; break;
+                        case 3: way = new int[] { -1, 0 }; break;
+                        case 4: way = new int[] { 1, 1 }; break;
+                        case 5: way = new int[] { -1, 1 }; break;
+                        case 6: way = new int[] { 1, -1 }; break;
+                        case 7: way = new int[] { -1, -1 }; break;
+                    }
+
+                    for(int j = 4; j > 0; j--)
+                    {
+
+                    }
+
+                }
+            }
+        }
+
+        public void win(value winner)
+        {
 
         }
+
     }
 }
