e30b969024a329ad292249c9b79bc262f1580cf4
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2023-03-06 12:50:45 +0100

    Add project files.

---
+++ Pyskovrky_more_2/Game.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pyskovrky_more_2
+{
+    internal class Game
+    {
+        public List<fullfield> FieldList;
+        public value round;
+
+        public Game(List<fullfield> fieldList, value round)
+        {
+            if (fieldList == null)
+            {
+                FieldList = new List<fullfield>();
+            }
+            else
+            {
+                FieldList = fieldList;
+            }
+
+            if (round == null)
+            {
+                this.round = value.X;
+            }
+            else
+            {
+                this.round = round;
+            }
+
+
+        }
+
+        public void add_field(fullfield temp)
+        {
+            if (temp.val == round && FieldList.Exists(x => (x.y == temp.y) && (x.x == temp.x)))
+            {
+                FieldList.Add(temp);
+            }
+            else
+            {
+                MessageBox.Show("Tam teď ne!");
+            }
+        }
+    }
+}
---
+++ Pyskovrky_more_2/fullfield.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyskovrky_more_2
+{
+    internal class fullfield
+    {
+        public int x;
+        public int y;
+
+        public value val;
+
+        public fullfield(int x, int y, value n) 
+        {
+            this.x = x;
+            this.y = y;
+            this.val = n;
+        }
+    }
+
+    public enum value
+    {
+        X,
+        Y
+    }
+}
